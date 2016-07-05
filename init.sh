6a888dcb96ea9021f83f94367186de3365b2e5c4
Charles Feval <chfev@ip-192-168-12-115.us-west-2.compute.internal>
2016-07-05 09:37:21 -0700

    Init a repo

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPO=$1
+
+if [ ! $REPO ]; then
+    echo "Usage: init.sh REPONAME"
+    exit 1
+fi
+
+echo "Creating directories"
+
+DIRECTORIES="bin test src"
+
+for DIRECTORY in $DIRECTORIES; do
+    [ ! -d $REPO/$DIRECTORY ] && mkdir $REPO/$DIRECTORY
+done
+
+echo "Copying files"
+
+cp src/*.sh $REPO/
+
+FILES="package.json.template version.txt"
+
+for FILE in $FILES; do
+    [ ! -s $REPO/$FILE ] && cp bin/$FILE $REPO/$FILE
+done
