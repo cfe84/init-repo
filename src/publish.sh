aa4e383051f3a0cea60e0e95a2fcd9a2cfe36820
Charles Feval <chfev@ip-192-168-12-115.us-west-2.compute.internal>
2016-07-05 09:57:33 -0700

    Copy readme on publish

--- src/publish.sh
+++ src/publish.sh
@@ -9,6 +9,8 @@ git describe --long | sed -E s/[^-]*-\([0-9]+\)-.*/\\1/ > revision.txt
 REVISION=`cat revision.txt`
 VERSION=`cat version.txt`
 
+cp README.md bin/
+
 cat package.json.template | sed s/%VERSION%/$VERSION.$REVISION/g > bin/package.json
 
 cd bin
